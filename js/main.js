b6f4199a66dafdcc218fa86e3260870b9e58edb9
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-08-05 19:07:25 -0700

    08052021-update

--- js/main.js
+++ js/main.js
@@ -623,8 +623,9 @@
       var submitBtnText = submitBtn.text();
       var feedbackEl = form.find('.contact-feedback');
       event.preventDefault();
+      feedbackEl.addClass('success').html('Thank you for your message. It has been sent.').fadeIn(200);
       // Waiting for the response from the server
-      submitBtn.html('Wait...').addClass('wait').prop('disabled', true);
+      //submitBtn.html('Wait...').addClass('wait').prop('disabled', true);
       setTimeout(function() {
         // Posts the Form's data to the server using Ajax
         $.ajax({
@@ -637,7 +638,7 @@
             // If the PHP file succeed sending the message
             if (response == 'success') {
               // Feedback to the user
-              submitBtn.removeClass('wait').html('Success').addClass('success');
+              //submitBtn.removeClass('wait').html('Success').addClass('success');
               feedbackEl.addClass('success').html('Thank you for your message. It has been sent.').fadeIn(200);
               setTimeout(function() {
                 submitBtn.html(submitBtnText).removeClass('success').prop('disabled', false);
